639b4d24c6e07458f65d3fd72dba254e457bfd65
Chirs Greene <chigreenwork@gmail.com>
2023-08-04 11:26:19 -0400

    cleaned up the code

--- assets/script.js
+++ assets/script.js
@@ -51,35 +51,5 @@ function generatePassword(){
     password += charset[Math.floor(Math.random() * n)]
   }
   
-  //we want a loop that will run the number of times that is determined by passwordLength. Each time the loop runs it selects a random variable from out master array. 
-  
-  //The master array is composed of the smaller arrays that are chosen by the choice prompt.
-  
-  
-  
   return password;
 }
-// generatePassword()
-
-
-
-
-
-// function generatePassword () {
-
-
-
-
-
-
-
-/*
-HOW TO GET BUTTONS TO PROVIDE PROMPTS
-  you could use a confirm button to ask for each character type.
-  use the true false output to add a string to the 
-
-HOW TO RANDOMLY SELECT THE NUMBER OF IN PUTS IT WANTS FROM THE STRING
-
-
-*/
-
